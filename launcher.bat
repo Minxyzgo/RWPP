f1028b20973238daf6ba73825c0e54e04cb5ccee
Dr <dr@der.kim>
2024-02-21 17:45:41 +0800

    Matching prefixes are implemented [Rwpp-windows]

--- launcher.bat
+++ launcher.bat
@@ -1,5 +1,35 @@
+@echo off
+setlocal enabledelayedexpansion
 
-java -D"java.net.preferIPv4Stack=true" -Xmx2000M -D"file.encoding=UTF-8" -D"prism.allowhidpi=false" -D"sun.java2d.uiScale=1" -D"java.library.path=." -cp "libs/*" io.github.rwpp.desktop.MainKt
+set DestPath=%~dp0
+set DestExt=*.jar*
 
+set /a count=0
+set /a lastName=0
+
+for /f "usebackq delims=" %%a in (`dir /b/a-d/s  "%DestPath%\%DestExt%"`) do (
+set "name=%%~na"
+set "c=!name:~0,12!"
+if "!c!"=="Rwpp-windows" (
+set /a count+=1
+set "lastName=%%a"
+)
+)
+
+if %count% EQU 0 (
+	echo "No RWPP-windows file exists"
+	pause
+	exit
+)
+
+if %count% GTR 1 (
+	echo "Multiple RWPP-windows file exists"
+	pause
+	exit
+)
+
+if %count% EQU 1 (
+	java -D"java.net.preferIPv4Stack=true" -Xmx2000M -D"file.encoding=UTF-8" -D"prism.allowhidpi=false" -D"sun.java2d.uiScale=1" -D"java.library.path=." -cp "!lastName!;libs/*" io.github.rwpp.desktop.MainKt
+)
 
 pause
