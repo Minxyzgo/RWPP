c7bdbef9926cd6f6be227a46a63ac38242ab7f27
Minxyzgo <1225327866@qq.com>
2025-08-01 01:11:40 +0800

    feat: 添加自托管MSI生成功能

--- wix/Program.cs
+++ wix/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Microsoft.Win32;
 using RSetup.RDialogs;
 using WixSharp;
-using WixToolset.Dtf.WindowsInstaller;
 using File = System.IO.File;
 
 namespace RSetup
@@ -45,7 +46,8 @@ namespace RSetup
                 new Dir( @"%ProgramFiles%\Minxyzgo\RWPP", entities));
 
             project.SourceBaseDir = RootDir;
-            
+          //  project.Scope = InstallScope.perMachine;
+            project.Platform = Platform.x64;
             project.DefaultFeature = appFeature;
             //此部分由gradle task执行，提供guid和version
             if (args.Length == 2)
@@ -98,9 +100,17 @@ namespace RSetup
             project.BeforeInstall += Msi_BeforeInstall;
             project.AfterInstall += Msi_AfterInstall;
             
+            //project.MajorUpgradeStrategy = MajorUpgradeStrategy.Default;
+            
             project.DefaultRefAssemblies.Add($@"{RootDir}\wix\bin\debug\net472\Wpf.Ui.dll");
 
-            project.BuildMsi();
+            var msiFile = project.BuildMsi();
+            var msiExe = Path.GetFullPath($@"{RootDir}\RWPP-Setup.exe");
+
+            (int exitCode, string output) = msiFile.CompleSelfHostedMsi(msiExe);
+
+            if (exitCode != 0)
+                Console.WriteLine(output);
         }
 
         private static void Msi_Load(SetupEventArgs e)
@@ -133,7 +143,7 @@ namespace RSetup
         private static void Msi_AfterInstall(SetupEventArgs e)
         {
             
-            if (e.Session.IsFeatureEnabled("Steam"))
+            if (e.Session.IsFeatureEnabled("Steam") && !e.IsUninstalling)
             {
                 var sourcePath = $@"{e.InstallDir}\Rusted Warfare - 64.exe";
                 var targetPath = $@"{e.InstallDir}\Rusted Warfare - 64.exe.bak";
@@ -187,6 +197,122 @@ namespace RSetup
             }
 
             return null;
+        }
+    }
+}
+
+
+static class ExeGen
+{
+    public static (int exitCode, string output) CompleSelfHostedMsi(this string msiFile, string outFile)
+    {
+        //System.Diagnostics.Debug.Assert(false);
+        var csc = LocateCsc();
+        var csFile = GenerateCSharpSource(outFile + ".cs");
+        
+        try
+        {
+            var extraArg = "/define:DEBUG /debug+";
+            Console.WriteLine($"Building: {outFile}");
+            return csc.Run($"\"/res:{msiFile}\" \"-out:{outFile}\" {extraArg}  /t:winexe \"{csFile}\"", Path.GetDirectoryName(outFile));
+        }
+        finally
+        {
+            File.Delete(csFile);
+        }
+    }
+
+    static string LocateCsc() =>
+        Directory.GetFiles(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), @"Microsoft.NET\Framework"), "csc.exe", SearchOption.AllDirectories)
+            .OrderByDescending(x => x)
+            .FirstOrDefault();
+
+    static string GenerateCSharpSource(string file)
+    {
+        var code = @"
+using System;
+using System.Resources;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        string msi = Path.GetTempFileName();
+       
+        try
+        {
+            // Debug.Assert(false); 
+            ExtractMsi(msi);
+           //  string msi_args = args.Any() ? string.Join("" "", args) : ""/i"";
+          
+            Process p = Process.Start(""msiexec.exe"", ""/i "" + msi);
+            p.WaitForExit();
+            return p.ExitCode;
+        }
+        catch (Exception)
+        {
+            // report the error
+            return -1;
+        }
+        finally
+        {
+            try
+            {
+                if (File.Exists(msi))
+                    File.Delete(msi);
+            }
+            catch { }
+        }
+    }
+
+    static void ExtractMsi(string outFile)
+    {
+        Assembly asm = Assembly.GetExecutingAssembly();//19724634308
+
+        using (Stream stream = asm.GetManifestResourceStream(""RWPP.msi""))
+        {
+            if (stream != null)
+            {
+                byte[] resourceBytes = new byte[stream.Length];
+                stream.Read(resourceBytes, 0, resourceBytes.Length);
+
+                File.WriteAllBytes(outFile, resourceBytes);
+            }
+            else
+            {
+                Console.WriteLine(""Resource not found."");
+            }
+        }
+    }
+}";
+        File.WriteAllText(file, code);
+        return file;
+    }
+
+    static (int exitCode, string output) Run(this string exe, string arguments, string workingDir)
+    {
+        using (var process = new Process())
+        {
+            process.StartInfo.FileName = exe;
+            process.StartInfo.Arguments = arguments;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.WorkingDirectory = workingDir;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            var output = new StringBuilder();
+
+            output.AppendLine(process.StandardOutput.ReadToEnd());
+            output.AppendLine(process.StandardError.ReadToEnd());
+
+            process.WaitForExit();
+            return (process.ExitCode, output.ToString());
         }
     }
 }
