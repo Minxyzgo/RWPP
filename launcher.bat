1649c93db65be1f0f01e56014a5323f592180470
Minxyzgo <1225327866@qq.com>
2025-08-01 11:53:05 +0800

    feat: 添加多平台启动脚本

--- launcher.bat
+++ launcher.bat
@@ -10,26 +10,26 @@ set /a lastName=0
 for /f "usebackq delims=" %%a in (`dir /b/a-d/s  "%DestPath%\%DestExt%"`) do (
 set "name=%%~na"
 set "c=!name:~0,12!"
-if "!c!"=="RWPP-windows" (
+if "!c!"=="RWPP-multiplatform" (
 set /a count+=1
 set "lastName=%%a"
 )
 )
 
 if %count% EQU 0 (
-	echo "No RWPP-windows file exists"
+	echo "No RWPP-multiplatform file exists"
 	pause
 	exit
 )
 
 if %count% GTR 1 (
-	echo "Multiple RWPP-windows file exists"
+	echo "Multiple RWPP-multiplatform file exists"
 	pause
 	exit
 )
 
 if %count% EQU 1 (
-	java -D"java.net.preferIPv4Stack=true" -Xmx2000M -D"file.encoding=UTF-8" -D"prism.allowhidpi=false" -D"java.library.path=." -cp "!lastName!;libs/*" io.github.rwpp.desktop.MainKt
+	java -D"java.net.preferIPv4Stack=true" -Xmx2000M -D"file.encoding=UTF-8" -D"prism.allowhidpi=false" -D"java.library.path=." -cp "!lastName!;generated_lib/*;libs/*" io.github.rwpp.desktop.MainKt
 )
 
 pause
